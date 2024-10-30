e81568a1402a71b6bd2d5d1a8b669e88a0f3aa58
RStuckey1 <stuckey85@gmail.com>
2024-10-30 14:01:09 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Card from './Card.jsx'
-import Card2 from './Card2.jsx'
-import About from './Pages/About.jsx'
-import Contact from './Pages/Contact.jsx'
-import Landing from './Pages/Landing.jsx'
-import Portfolio from './Pages/Portfolio.jsx'
-import Resume from './Pages/Resume.jsx'
-import './index.css'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
 
 function App() {
-
-  return(
-     <>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Landing/>}/> 
-            <Route path='/About' element={<About/>}/>
-            <Route path='/Contact' element={<Contact/>}/>
-            <Route path='/Card2' element={<Card2/>}/>
-            <Route path='/Header' element={<Header/>}/>
-            <Route path='/Footer' element={<Footer/>}/>
-            <Route path='/Card' element={<Card/>}/>
-            <Route path='/Portfolio' element={<Portfolio/>}/>
-            <Route path='/Resume' element={<Resume/>}/>
-          </Routes>
-        </Router>  
+  return <div className="portfolio-app">
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  ); 
+  </div>
 }
 
- 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,63 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Card from './Card.jsx'
+import Card2 from './Card2.jsx'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
+import Portfolio from './Pages/Portfolio.jsx'
+import Resume from './Pages/Resume.jsx'
+import ErrorPage from './Pages/ErrorPage.jsx'
+import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <App/>,
+  errorElement: <ErrorPage />,
+  children: [
+    {
+      path: '/About',
+      element: <About />,
+    },
+    {
+      path: '/Contact',
+      element: <Contact />,
+    },
+    {
+      path: '/Card2',
+      element: <Card2 />,
+    },
+    {
+      path: '/Header',
+      element: <Header />,
+    },
+    {
+      path: '/Footer',
+      element: <Footer />,
+    },
+    {
+      path: '/Card',
+      element: <Card />,
+    },
+    {
+      path: '/Portfolio',
+      element: <Portfolio />,
+    },
+    {
+      path: '/Resume',
+      element: <Resume />,
+    },
+  ]
+}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
