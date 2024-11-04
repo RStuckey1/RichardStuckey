c6db0db796365a7458d1530a106160bfa9bc938d
RStuckey1 <stuckey85@gmail.com>
2024-11-04 17:29:13 -0600

    update

--- src/Card4.jsx
+++ src/Card4.jsx
@@ -6,7 +6,7 @@ function Card4(){
    return(
     <Link to="https://github.com/RStuckey1/Car-Pal-" target="_blank">
         <div className="card2">
-            <img className="card-image2" src={carpalapp} alt="profile picture"></img>
+            <img className="card-image2" src={carpalapp} alt="car pal icon"></img>
             <h2 className='card-title2'>Car Pal App</h2>
             <p className='card-text2'>vehicle maintenance tracker</p>
         </div>
