6e10c20eb533112d413e6f534770f293c9b8b62e
RStuckey1 <stuckey85@gmail.com>
2025-01-10 11:24:48 -0600

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,19 +8,22 @@ function About() {
     <>
     <div className="about">
       <h1>About Richard Stuckey</h1>
-      <h2 class="about">Richard "Rick" Stuckey is mechanically inclined, yet uniquely creative and practical with solutions. </h2>
-      <h2 class="about">Technology is a conduit for these soluitions to generate efficent and sustainable remedies for any problem. </h2>
       <h2 class="about"></h2>
-      <h2 class="about">My life is a gift that I am thankful for and will never waste.</h2>
-      <h2 class="about">The forgotten lesson is to have fun, you only have one go at life.</h2>
+      <h2 class="about">My life, so far, is an amazing story. If it were a book, readers would be on the edge of their seat, unable to put it down.</h2>
+      <h2 class="about">Being tenacious and curious has been the driving force in my life, with many unique chapters. I have learned to solve unique problems with a multidisciplinary approach
+      using technology, sustaionability, creativity, and teamwork. In every chapter, the people I have met have been the greatest resource because every person you meet knows something you don't.  
+      </h2>
+     <p>I have always been facinated with the relationship between technology and nature.</p>
     </div>
   <div className="aboutPics">
     <img class="pic" src={RickBees} alt="Rick Bees"></img>
-    <p>Studying Apiculture(Bees)</p>
+    <p>Studying Apiculture -University of Kansas (Lawrence, Kansas)</p>
   </div>
   <div className="aboutPics">
     <img className="pic" src={Penguin} alt="Penguin"></img>
-    <p>My favorite animal</p>
+    <p>Kansas City Zoo & Aquarium</p>
+    <p>A quick pic with King Penguin "Buddy Love"</p>
+    <p>On the way to my office (2019)</p>
   </div>
   <div class="aboutPics">
     <img class="pic" src={BW} alt="Black and White"></img>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import strobe from '../assets/strobulation.gif'
 import './Home.css';
+
 
 
 function Home() {
   return (
     <>
     <div className="home">
-      <p6>Experiment</p6>
-      <p6>Fail</p6>
-      <p6>Learn</p6>
-      <p6>Repeat</p6>
+      <p class="quote">Experiment   Fail     Learn    Repeat</p>
       <br></br>
       <br></br>
-      <h7>Twenty years from now you will be more disappointed by the things</h7>
-      <h7>that you didn't do than by the ones you did do,</h7>
-      <h7>so throw off the bowlines, sail away from safe harbor,</h7>
-      <h7>catch the trade winds in your sails. Explore. Dream. Discover.</h7>
-      <h7>~Mark Twain</h7>
-     
+      <h7>"Twenty years from now you will be more disappointed by the things
+      that you didn't do than by the ones you did do,
+      so throw off the bowlines, sail away from safe harbor,
+      catch the trade winds in your sails. Explore. Dream. Discover."
+      ~Mark Twain</h7>
+      
+    <div className="strobe">
+        <img class="strobePic" src={strobe} alt="Jellyfish Strobulation in Captivity"></img>
+    </div>
     </div>
     </>
   );
