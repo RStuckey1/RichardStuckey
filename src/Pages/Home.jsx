7cff1ef3190e8f37b608b72f5d8c9714e866ae17
RStuckey1 <stuckey85@gmail.com>
2025-01-16 13:00:58 -0600

    resume

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,12 +9,22 @@ function Home() {
   return (
     
     <div className="home">
-      <p class="intro">Welcome, I am glad you found your way here. There are many things to see here. 
-        What you see below are my inspirations, accomplishments, and dreams. Above you 
+      <p class="intro">Welcome, I am glad you found your way here to my Website. This place is a glimpse into
+        my world. Above you can click any of the following: <br></br>
+        <ul>
+          <li>About - Get to know me</li>
+          <li>Resume - How I look on paper</li>
+          <li>Portfolio - Projects and other Fun</li>
+          <li>Contact - How to reach me. I would love to hear from you!</li>
+          <li>Rick Stuckey - Brings you back to here</li>
+        </ul>
+        
+
+        inspirations, accomplishments, and dreams. Above you 
         can click any of the links to see more about me, my experience, and projects of mine. 
         On my contact page you can send me a messae, which I always enjoy and will respond. 
         Thank you for being here, and I hope you enjoy your visit. </p>
-      <p><span>Experiment</span> <span>Fail</span> <span>Learn</span> <span>Repeat</span></p>
+
       <br></br>
       <br></br>
       <h7>"Twenty years from now you will be more disappointed by the things
@@ -28,16 +38,16 @@ function Home() {
     
     <div className="strobe">
         <img class="strobePic" src={strobe} alt="Jellyfish Strobulation in Captivity"></img>
-        <p>Jellyfish during strobulation. The pulsating tree looking polyp is a 
+        <p class="strobe">Jellyfish during strobulation. The pulsating tree looking polyp is a 
           stack of several jellyfish about to break off and grow as individual jellyfish.</p>
-
-          <div class="strobe">
+    </div>
+    <div classname="strobe">
     <img class="pic" src={BW} alt="Black and White"></img>
     <p>Boundary Waters, USA/Canada 2024</p>
   </div>
         
     </div>
-    </div>
+    
     
   );
 }   
--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -13,6 +13,15 @@ import python from '../assets/icons/python.png';
 import git from '../assets/icons/git.png';
 import github from '../assets/icons/github.png';
 import reactpic from '../assets/icons/reactpic.png';
+import arduino from '../assets/icons/arduino.png';
+import graphql from '../assets/icons/graphql.png';
+import jsonwebtoken from '../assets/icons/jsonwebtoken.png';
+import excel from '../assets/icons/excel.png';
+import word from '../assets/icons/word.png';
+import npm from '../assets/icons/npm.png';
+import postgresql from '../assets/icons/postgresql.png';
+import raspberrypi from '../assets/icons/raspberrypi.png';
+import materialui from '../assets/icons/materialui.png';
 import './Resume.css';
 
 
@@ -24,77 +33,108 @@ const Item = styled(Box)(({ theme }) => ({
     color: theme.palette.text.secondary,
     ...theme.applyStyles('darkj', {
         backgroundColor: '#000000',
-        }),
-    }));
-
-
+    }),
+}));
 
 
 function Resume() {
-    return(
-        <>
-        <div className="codingSkills">
-            <Stack direction="row" spacing={2}
-                sx = {{
-                    width: '100%',
-                    height: '100%'
+    return (
+        <body>
+        <h1>coding skills</h1>
+     
+            <div className="codingSkills" id="codebox" >
+                
+                <Stack direction="row" spacing={6}
+                    sx={{
+                        width: '100%',
+                        height: '100%'
                     }}>
-                <Item><img src={css} alt="css" /></Item>
-                <Item><img src={html} alt="html" /></Item>
-                <Item><img src={js} alt="js" /></Item>
-                <Item><img src={ts} alt="ts" /></Item>
-            </Stack>
-            <Stack direction="row" spacing={2}>
-                <Item><img src={github} alt="github" /></Item>
-                <Item><img src={reactpic} alt="reactpic" /></Item>
-                <Item><img src={node} alt="node" /></Item>
-                <Item><img src={mongo} alt="mongo" /></Item>
-            </Stack>
-            <Stack direction="row" spacing={2}>
-                <Item><img src={sql} alt="sql" /></Item>
-                <Item><img src={python} alt="python" /></Item>
-                <Item><img src={git} alt="git" /></Item>
-               
-            </Stack>
-        </div>
-        <div className="resume">
-            <h1 className="resume">Resume</h1>
-            <h2>Coding Knowledge</h2>
+                    
+                    <Item><img src={css} alt="css" /></Item>
+                    <Item><img src={html} alt="html" /></Item>
+                    <Item><img src={js} alt="js" /></Item>
+                    <Item><img src={ts} alt="ts" /></Item>
+                </Stack>
+                <Stack direction="row" spacing={6}
+                    sx={{
+                        width: '100%',
+                        height: '100%'
+                    }}>
+                    <Item><img src={github} alt="github" /></Item>
+                    <Item><img src={reactpic} alt="reactpic" /></Item>
+                    <Item><img src={node} alt="node" /></Item>
+                    <Item><img src={mongo} alt="mongo" /></Item>
+                </Stack>
+                <Stack direction="row" spacing={6}
+                    sx={{
+                        width: '100%',
+                        height: '100%'
+                    }}>
+                    <Item><img src={sql} alt="sql" /></Item>
+                    <Item><img src={python} alt="python" /></Item>
+                    <Item><img src={git} alt="git" /></Item>
+                    <Item><img src={arduino} alt="arduino" /></Item>
+                </Stack>
+                <Stack direction={"row"} spacing={6}
+                    sx={{
+                        width: '100%',
+                        height: '100%'
+                    }}>
+                    <Item><img src={graphql} alt="graphql" /></Item>
+                    <Item><img src={jsonwebtoken} alt="jsonwebtoken" /></Item>
+                    <Item><img src={excel} alt="excel" /></Item>
+                    <Item><img src={word} alt="word" /></Item>
+                </Stack>
+                <Stack direction={"row"} spacing={6}
+                    sx={{
+                        width: '100%',
+                        height: '100%'
+                    }}>
+                    <Item><img src={npm} alt="npm" /></Item>
+                    <Item><img src={postgresql} alt="postgresql" /></Item>
+                    <Item><img src={raspberrypi} alt="raspberrypi" /></Item>
+                    <Item><img src={materialui} alt="materialui" /></Item>
+                </Stack>
+            </div>
+            <div className="resume">
+                <h1 className="resume">Resume</h1>
+                <h2>Coding Knowledge</h2>
 
-            <h2>Experience Highlights</h2>
-            <h3>Kansas City Zoo & Aquarium (Life Support Systems Manager)</h3>
-            <h4 className="resume">SOBELLA Oceanarium</h4>
-            <p>From conceptual meetings to opening, I contributed to the design and commissioning of the Life Support Systems and the building</p>
-            <p>management controls systems.</p>
-            <h4>Stingray Bay</h4>
-            <p>This was my first ground-up touch tank exhibit. I contributed to the design, installation, and commissioning of the exhibit.</p>
-            <p>I managed the Life Support Systems, and the building automation systems. All of the sensors and controllers utilized remote monitoring</p>
-            <p>via a web applicaion interface</p>
-            <h4>Helzberg Penguin Plaza</h4>
-            <p>This building had many manual procedures that I upgraded to remote monitoring/controls. This included the ozone system for three large</p>
-            <p>aquariums, flowmeters for both a cold penguin(120k gallons) and warm pengiun(30k gallons) exhibits.</p>
-            <p>During this time I identified many processes and procedures that could be improved.</p>
-            <p>Examples:</p>
-            <p>-Changing exhibit salt suppliers, saving $65,000 per year</p>
-            <p>-Cutting out middleman supply houses, buying parts from manufacture for pumps, ozone equipment, etc.</p>
-            <p>saving over $30,000 per year.</p>
-           
-            <h2>Education</h2>
-            <p>University of Kansas(Lawrence, KS) </p>
-            <p>Bachelor's of Science - Environmental Science</p>
-            <p>Bachelor's of Science - Microbiology</p>
-            <p>Minor - Biological Anthropology</p>
-            <h2>Work History</h2>
-            <p>Cogent (Controls Field Technician)2023-2024</p>
-            <p>Kansas City Zoo and Aquarium (Life Support Systems/ Water Quality Manager)2015-2023</p>
-            <p>Landry's T-REX Cafe (Curator/ Aqusition&Quarantine Manager)2008-2015</p>
-            <p>Environmental Science Coporation (Field Technician)2007-2008</p>
-            
-            
-            
-        </div>
-        </>
+                <h2>Experience Highlights</h2>
+                <h3>Kansas City Zoo & Aquarium (Life Support Systems Manager)</h3>
+                <h4 className="resume">SOBELLA Oceanarium</h4>
+                <p>From conceptual meetings to opening, I contributed to the design and commissioning of the Life Support Systems and the building</p>
+                <p>management controls systems.</p>
+                <h4>Stingray Bay</h4>
+                <p>This was my first ground-up touch tank exhibit. I contributed to the design, installation, and commissioning of the exhibit.</p>
+                <p>I managed the Life Support Systems, and the building automation systems. All of the sensors and controllers utilized remote monitoring</p>
+                <p>via a web applicaion interface</p>
+                <h4>Helzberg Penguin Plaza</h4>
+                <p>This building had many manual procedures that I upgraded to remote monitoring/controls. This included the ozone system for three large</p>
+                <p>aquariums, flowmeters for both a cold penguin(120k gallons) and warm pengiun(30k gallons) exhibits.</p>
+                <p>During this time I identified many processes and procedures that could be improved.</p>
+                <p>Examples:</p>
+                <p>-Changing exhibit salt suppliers, saving $65,000 per year</p>
+                <p>-Cutting out middleman supply houses, buying parts from manufacture for pumps, ozone equipment, etc.</p>
+                <p>saving over $30,000 per year.</p>
+
+                <h2>Education</h2>
+                <p>University of Kansas(Lawrence, KS) </p>
+                <p>Bachelor's of Science - Environmental Science</p>
+                <p>Bachelor's of Science - Microbiology</p>
+                <p>Minor - Biological Anthropology</p>
+                <h2>Work History</h2>
+                <p>Cogent (Controls Field Technician)2023-2024</p>
+                <p>Kansas City Zoo and Aquarium (Life Support Systems/ Water Quality Manager)2015-2023</p>
+                <p>Landry's T-REX Cafe (Curator/ Aqusition&Quarantine Manager)2008-2015</p>
+                <p>Environmental Science Coporation (Field Technician)2007-2008</p>
+
+
+
+            </div>
+   
+    </body>
     );
 }
 
-    export default Resume;
+export default Resume;
