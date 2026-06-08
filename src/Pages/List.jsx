4f9f7e67912b363546bd35096442397fb2029b4e
RStuckey1 <stuckey85@gmail.com>
2026-06-08 01:27:36 -0500

    updated

--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -9,7 +9,7 @@ export function List() {
         <>
         <div className="List">
             <div className="portfolio">
-                <h1>Rick's Portfolio</h1>
+                <h1>Rick's Coding Projects</h1>
                 <p>Click on the pictures to experience some of my coding projects</p>
             </div>
             <div className="bottom">
